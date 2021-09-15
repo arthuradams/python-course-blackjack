f49436a07d6632c5f22a84703bb8a520504dd19b
Arthur C. Adams <arthuradams@gmail.com>
2021-09-15 14:54:23 -0400

    Added logic to actualy play the game, and other stuff I should have commited by now

--- blackjack.py
+++ blackjack.py
@@ -5,10 +5,52 @@ my_deck = cards.Deck()
 
 my_deck.shuffle()
 
-player1 = players.Player("Name")
-dealer = players.Player("Dealer")
+player1 = players.Player("Arthur", True)
+dealer = players.Player("Dealer", False)
 
-for num in range(0,3):
+for num in range(0,2):
     player1.add_card(my_deck.deal_one())
+    dealer.add_card(my_deck.deal_one())
 
 print(player1)
+print(dealer)
+
+player_bust = False
+dealer_bust = False
+player_win = False
+dealer_win = False
+
+while True:
+    if player1.hand_total() > 21:
+        print( "Bust" )
+        player_bust = True
+        break
+    choice = input(f"{player1.name}, do you want to (H) or (S)tand: ")
+    if choice[0].upper() == "S":
+        break
+    elif choice[0].upper() == "H":
+        player1.add_card(my_deck.deal_one())
+        print(player1)
+    else:
+        print("Invalid selection")
+
+if player_bust:
+    print(f"{player1.name} loses")
+    print(player1)
+
+dealer.show_first_card = True
+
+while dealer.hand_total() < 17:
+    dealer.add_card(my_deck.deal_one())
+    print(dealer)
+
+if dealer.hand_total() > 21:
+    print("Dealer bust")
+    dealer_bust = True
+elif player1.hand_total() > dealer.hand_total():
+    print( f"{player1.name} wins")
+else:
+    print( f"{dealer.name} wins")
+
+
+    
--- players.py
+++ players.py
@@ -1,19 +1,28 @@
 class Player:
     
-    def __init__(self,name):
+    def __init__(self,name, show_first_card=True):
         self.name = name
+        self.show_first_card = show_first_card
         # A new player has no cards
         self.hand = [] 
         
     def add_card(self,new_card):
         self.hand.append(new_card)
     
-    
     def __str__(self):
-#        card_list = []
-#        for card in self.hand:
-#            card_list.append(str(card))
-        return f'Player {self.name} has {", ".join(self.hand)} with a value of {self.hand_total()}.'
+        card_list = []
+        for card in self.hand:
+            if len(card_list) == 0 and not self.show_first_card:
+                card_list.append("Face down")
+            else:
+                card_list.append(str(card))
+
+        outstr = f'{self.name} has {", ".join(card_list)}'
+
+        if self.show_first_card:
+            outstr += f" has a value of {self.hand_total()}"
+
+        return outstr
 
     def hand_total(self):
         total = 0
